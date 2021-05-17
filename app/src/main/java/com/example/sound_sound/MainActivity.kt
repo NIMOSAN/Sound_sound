9fa76e18712c3c0099d80530cca2418b37e99d96
NIMOSAN <mogami0916r@gmail.com>
2021-05-17 22:25:25 +0900

    edit

--- app/src/main/java/com/example/sound_sound/MainActivity.kt
+++ app/src/main/java/com/example/sound_sound/MainActivity.kt
@@ -1,22 +1,121 @@
 package com.example.sound_sound
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.core.app.ActivityCompat
 import androidx.core.view.get
 import java.io.IOException
 
+private const val LOG_TAG = "AudioRecordTest"
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+
 class MainActivity : AppCompatActivity() {
     private var _player: MediaPlayer? = null
+    private var recorder: MediaRecorder? = null
+    private var fileName: String = ""
+
+
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
+    }
+
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
+    }
+
+    private fun startPlaying() {
+        _player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+        }
+    }
+
+    private fun stopPlaying() {
+        _player?.release()
+        _player = null
+    }
+
+
+
+
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Sound_sound)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+
+        val record = findViewById<Button>(R.id.btRecord) //録音オブジェクト取得
+        val stop = findViewById<Button>(R.id.btStop) //録音停止オブジェクト取得
+        val playback = findViewById<Button>(R.id.btPlay2) //再生オブジェクト取得
+
+        val listener = RecordButton() //レコードボタンリスナのインスタンス生成
+
+        record.setOnClickListener(listener) //レコードボタンリスナの設定
+        stop.setOnClickListener(listener)
+        playback.setOnClickListener(listener)
+
         val lvSound = findViewById<ListView>(R.id.lvSound)
         lvSound.onItemClickListener = ListItemClickListener()
         val loopSwitch = findViewById<Switch>(R.id.swLoop)
@@ -133,6 +232,32 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "メディアプレイヤー準備時にエラー.もう一度試すにも.", Toast.LENGTH_SHORT).show()
             }
             Toast.makeText(applicationContext,item,Toast.LENGTH_SHORT).show()
+        }
+    }
+    private inner class RecordButton : View.OnClickListener {
+        override fun onClick(v: View?) {
+            Log.i(LOG_TAG, "クリック成功")
+            Log.i(LOG_TAG, fileName)
+
+            if(v != null){
+                when(v.id){
+                    //録音開始ボタン
+                    R.id.btRecord -> {
+                        onRecord(true)
+                        Log.i(LOG_TAG, "録音開始")
+                    }
+                    //録音停止ボタン
+                    R.id.btStop -> {
+                        onRecord(false)
+                        Log.i(LOG_TAG, "録音終了")
+                    }
+
+                    R.id.btPlay2 -> {
+                        onPlay(true)
+                        Log.i(LOG_TAG, "再生中")
+                    }
+                }
+            }
         }
     }
 }
